e80fe35a03f4efc7ed319cc86aca74a702ffe79b
omaralhawamdeh97 <omar.alhawamdeh@gmail.com>
2021-06-11 20:01:42 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import cards from "./cards";
+
+const shuffledCards = cards.sort(() => Math.random() - 0.5);
+const shuffledCardsTwo = cards.sort(() => 0.5 * Math.random());
 
 function App() {
+  //states for opened cards and matched cards
+  const [openedCard, setOpenedCard] = useState([]);
+  const [matched, setMatched] = useState([]);
+
+  //pair of cards spreaded from the array so we can check them
+  const pairOfcards = [...shuffledCards, ...shuffledCardsTwo];
+
+  // function to flip the card and set it in opened card from the state
+  function flipCard(index) {
+    setOpenedCard((opened) => [...opened, index]);
+  }
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  useEffect(() => {
+    if (openedCard < 2) return;
+
+    const firstMatched = pairOfcards[openedCard[0]];
+    const secondMatched = pairOfcards[openedCard[1]];
+
+    if (secondMatched && firstMatched.id === secondMatched.id) {
+      setMatched([...matched, firstMatched.id]);
+    }
+
+    if (openedCard.length === 2) setTimeout(() => setOpenedCard([]), 1000);
+  }, [openedCard]);
+
+  console.log(matched.length);
+  const myFunction = () => alert("YOU WIN !!!!" + " ,Refresh for restart");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <td>
+          <div className="Score"> Score : {matched.length}</div>
+        </td>
+        <td>
+          <button className="ScoreTwo" onClick={refreshPage}>
+            Restart
+          </button>
+        </td>
+      </table>
+      <div className="cards">
+        {pairOfcards.map((card, index) => {
+          //lets flip the card
+
+          let isFlipped = false;
+
+          if (openedCard.includes(index)) isFlipped = true;
+          if (matched.includes(card.id)) isFlipped = true;
+          return (
+            <div
+              className={`card-card ${isFlipped ? "flipped" : ""} `}
+              key={index}
+              onClick={() => flipCard(index)}
+            >
+              <div className="inner">
+                <div className="front">
+                  <img
+                    src={`${card.src}`}
+                    alt="card-name"
+                    width="100%"
+                    margin="20px"
+                  />
+                </div>
+                <div className="back"></div>
+              </div>
+              {matched.length === 8 ? myFunction() : ""}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/cards.js
@@ -0,0 +1,44 @@
+let cards = [
+  {
+    id: 1,
+    name: "Ace",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/935/AS__68652.1440113599.1280.1280.png?c=2",
+  },
+  {
+    id: 2,
+    name: "Two",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/924/2D__57497.1440113502.1280.1280.png?c=2",
+  },
+  {
+    id: 3,
+    name: "Three",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/925/3C__99122.1440113509.1280.1280.png?c=2",
+  },
+  {
+    id: 4,
+    name: "Four",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/926/4H__83243.1440113515.1280.1280.png?c=2",
+  },
+  {
+    id: 5,
+    name: "Five",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/927/5S__90574.1440113521.1280.1280.png?c=2",
+  },
+  {
+    id: 6,
+    name: "Six",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/928/6D__92916.1440113530.1280.1280.png?c=2",
+  },
+  {
+    id: 7,
+    name: "Seven",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/929/7C__93490.1440113539.1280.1280.png?c=2",
+  },
+  {
+    id: 8,
+    name: "Eight",
+    src: "https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/930/8S__27839.1440113555.1280.1280.png?c=2",
+  },
+];
+
+export default cards;
